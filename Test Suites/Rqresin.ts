c34a36aaf18016f492ee41d45d250ebbc71bdb0f
rahdian-abdi <rahdianid@gmail.com>
2022-12-17 14:40:39 +0700

    add test suit

---
+++ Test Suites/Rqresin.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Rqresin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>11d6aff0-8870-46e5-8171-d951d706fe31</testSuiteGuid>
+   <testCaseLink>
+      <guid>f04628ab-78f1-46c6-a2f6-6e0bd37daf6f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get List User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1591d24d-6d44-48ce-bf38-a6f34cf95027</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Single User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>992cf8ba-e309-4422-b52c-80c592490098</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Update User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ad821da-3cf2-4f9f-a344-356b6e04f891</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create User</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
