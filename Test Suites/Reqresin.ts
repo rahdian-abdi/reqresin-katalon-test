ed949ae432f13349b271960e2e8fe5a04116fc71
rahdian-abdi <rahdianid@gmail.com>
2022-12-17 17:59:55 +0700

    fix

--- Test Suites/Reqresin.ts
+++ Test Suites/Reqresin.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Rqresin</name>
+   <name>Reqresin</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
